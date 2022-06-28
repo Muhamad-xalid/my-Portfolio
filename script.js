11eeb13a94b35c2726a9132c303166f68e37e544
Muhamad-xalid <muhamadxalidahmad00@gmail.com>
2022-06-28 12:57:04 +0300

    connect javascript file with html file

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function resize() {
-    let url = document.getElementById('url').value
-    let height = document.getElementById('height').value
-    let width = document.getElementById('width').value
+    var url = document.getElementById('url').value
+    var height = document.getElementById('height').value
+    var width = document.getElementById('width').value
+    // var img = document.getElementById('img')
+    // var urltext = document.getElementById('urltext')
+
+    // if (url == '' || height == '' || width == ''){
+   
+    
+        let lastindex = url.lastIndexOf('/');
+        let lastpart = url.subString(lastindex)
+
+        let generateurl = `https://source.unsplash.com${lastpart}/${width}x${height}`
+        alert(generateurl)
+    
 
 
 }
