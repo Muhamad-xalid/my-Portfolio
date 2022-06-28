14cfce01680e50c0aa8a90469b38f64079983448
Muhamad-xalid <muhamadxalidahmad00@gmail.com>
2022-06-28 11:23:00 +0300

    create new javascript file and create form

---
+++ script.js
@@ -0,0 +1,7 @@
+function resize() {
+    let url = document.getElementById('url').value
+    let height = document.getElementById('height').value
+    let width = document.getElementById('width').value
+
+
+}
