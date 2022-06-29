84c1c3795856372eadfc6f8527f52c23e18db0d6
Muhamad-xalid <muhamadxalidahmad00@gmail.com>
2022-06-29 12:48:13 +0300

    .

--- script.js
+++ script.js
@@ -2,18 +2,14 @@ function resize() {
     var url = document.getElementById('url').value
     var height = document.getElementById('height').value
     var width = document.getElementById('width').value
-    // var img = document.getElementById('img')
-    // var urltext = document.getElementById('urltext')
-
-    // if (url == '' || height == '' || width == ''){
-   
+  
     
         let lastindex = url.lastIndexOf('/');
         let lastpart = url.subString(lastindex)
 
         let generateurl = `https://source.unsplash.com${lastpart}/${width}x${height}`
         alert(generateurl)
-    
+
 
 
 }
